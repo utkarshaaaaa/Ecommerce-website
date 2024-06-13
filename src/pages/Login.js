200a13cbe14a837c71d9062389a7d39bc37f5360
utkarsh <vyasutkarsh11@gmail.com>
2024-06-13 13:31:25 +0530

    commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,14 +9,12 @@ import { useNavigate } from "react-router-dom";
 //username :kminchelle
 //password: '0lelplR'
 export default function Login() {
-  const {  setuserimg, datalist, setDatalist, userdata, setuserdata } = useContext(Data);
-   
+  const { setuserimg, datalist, setDatalist, userdata, setuserdata } =
+    useContext(Data);
 
   const [token, settoken] = useState();
 
   const navigate = useNavigate();
-
- 
 
   const handleChange = (e) => {
     const value = e.target.value;
@@ -38,17 +36,15 @@ export default function Login() {
         settoken(response.data.token);
         setuserimg(response.data.image);
         setuserdata(...userdata, [response.data]);
-        
 
         localStorage.setItem("login", true);
         navigate("/");
-      }).catch(error => {
+      })
+      .catch((error) => {
         if (error.response && error.response.status === 429) {
-         
-          console.log('Try again later');
+          console.log("Try again later");
         } else {
-          
-          console.log('Error:', error.message);
+          console.log("Error:", error.message);
         }
       });
   };
@@ -59,15 +55,17 @@ export default function Login() {
     }
   });
 
-  console.log(Boolean(token));
- 
+  const UserNamePassword = () => {
+    alert("Username: emilys   Password: emilyspass");
+    alert("Have Fun shopping  :)");
+  };
+
+  console.log(token);
 
   return (
     <div>
-     
       <div className="form-login">
         <form action="" class="form_main" method="POST" onSubmit={handleSubmit}>
-         
           <p className="heading">Login</p>
           <div className="inputContainer">
             <svg
@@ -80,13 +78,13 @@ export default function Login() {
             >
               <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z"></path>
             </svg>
+
             <input
               type="text"
-              
               name="username"
-                value={datalist.username}
-                onChange={handleChange}
-                required
+              value={datalist.username}
+              onChange={handleChange}
+              required
               class="inputField"
               id="username"
               placeholder="Username"
@@ -106,12 +104,10 @@ export default function Login() {
             </svg>
             <input
               type="password"
-             
               name="password"
               required
               value={datalist.password}
               onChange={handleChange}
-              
               class="inputField"
               id="password"
               placeholder="Password"
@@ -119,8 +115,8 @@ export default function Login() {
           </div>
 
           <button id="button">Submit</button>
-          <a className="forgotLink" href="#">
-            Forgot your password?
+          <a className="forgotLink" href="#" onClick={UserNamePassword}>
+            Click to know the user name and password
           </a>
         </form>
       </div>
